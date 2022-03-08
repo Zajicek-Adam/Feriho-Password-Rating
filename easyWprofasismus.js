3dc0ee736fba700216c4ffc7696181f5e07a11f2
Adam Zajíček <adam.zajicek.020@pslib.cz>
2022-03-08 13:20:12 +0100

    uwu nyaaaa

--- easyWprofasismus.js
+++ easyWprofasismus.js
@@ -1,4 +1,4 @@
-let possibleFeriRatings = ["Feri approves.", "Feri is not concerned", "Feri is only slightly concerned.", "Feri disapproves.", "Feri is going to commit hate crimes."]
+let possibleFeriRatings = ["Feri approves.", "Feri is sligthy pleased", "Feri is apolitical.", "Feri disapproves.", "Feri is going to commit hate crimes."] // 2 1 2
 let feriInput = document.getElementById("feri-password");
 let feriRates = document.getElementById("feri-rating");
 
@@ -18,29 +18,58 @@ class uwu {
 
 function FeriRating() {
     let points = 0;
+    let hasUppercase = false;
+    hasUppercase = UpperCaseRatingByFeri();
+    points = LengthRatingByFeri(points, hasUppercase);
+    hasUppercase = false;
+    uwu.nyah(points);
+    uwu.nyah(hasUppercase)
+}
+function LengthRatingByFeri(points, hasUppercase){
     let length = feriInput.value.length;
     if(length < 6){
         uwu.nya(feriRates, "Feri říká: heslo musí mít aspoň 6 znaků");
+        return
     }
     else{
         uwu.nya(feriRates, "Feri říká: heslo má dostatek znaků");
-        uwu.nyah(length);
-        return
     }
     let bonusLength = length - 6;
-    if(bonusLength > 0 && bonusLength < 4){
+    if(bonusLength > 0 && bonusLength < 6){
         // 1.25 * n
-        
+        if(hasUppercase){
+            return points = 1.25 * (bonusLength + 1);
+
+        }
+        else{
+            return points = 1.25 * bonusLength;
+        }
     }
-    else if(bonusLength >= 4 && bonusLength < 8){
+    else if(bonusLength >= 6 && bonusLength < 9){
         // 1.75 * n
+        if(hasUppercase){
+            return points = 1.75 * (bonusLength + 1);
+
+        }
+        else{
+            return points = 1.75 * bonusLength;
+        }
+    }
+    // 2 * n
+    if(hasUppercase){
+        return points = 2 * (bonusLength + 1);
+
     }
     else{
-        // 2 * n
+        return points = 2 * bonusLength;
     }
-    uwu.nyah(length);
 }
-
+function UpperCaseRatingByFeri(){
+    if(feriInput.value.toLowerCase() == feriInput.value){
+        return false;
+    }
+    return true
+}
 FeriRating();
 
 feriInput.addEventListener("input", e => {
