28be2f4422707b7b6fd4bc6001b84768478febcc
Adam Zajíček <adam.zajicek.020@pslib.cz>
2022-03-10 13:26:41 +0100

    Feri

--- easyWprofasismus.js
+++ easyWprofasismus.js
@@ -21,58 +21,99 @@ function FeriRating() {
     let hasUppercase = false;
     hasUppercase = UpperCaseRatingByFeri();
     points = LengthRatingByFeri(points, hasUppercase);
+    if (points == undefined) {
+        points = 0;
+    }
+    points += NumberRatingByFeri();
+    if (feriInput.value.length > 6) {
+        points += SpecialRatingSexByFeri();
+        //Swapping text rating
+        if(points < 5){
+            uwu.nya(feriRates, possibleFeriRatings[4]);
+        }
+        else if (points < 15) {
+            uwu.nya(feriRates, possibleFeriRatings[3]);
+        } 
+        else if(points < 25){
+            uwu.nya(feriRates, possibleFeriRatings[2]);
+        }
+        else if(points < 35){
+            uwu.nya(feriRates, possibleFeriRatings[1]);
+        }
+        else{
+            uwu.nya(feriRates, possibleFeriRatings[0]);
+        }
+    }
+
+
+
+    //Update score
     hasUppercase = false;
+
+
     uwu.nyah(points);
     uwu.nyah(hasUppercase)
 }
-function LengthRatingByFeri(points, hasUppercase){
+function LengthRatingByFeri(points, hasUppercase) {
     let length = feriInput.value.length;
-    if(length < 6){
+    if (length < 6) {
         uwu.nya(feriRates, "Feri říká: heslo musí mít aspoň 6 znaků");
         return
     }
-    else{
-        uwu.nya(feriRates, "Feri říká: heslo má dostatek znaků");
-    }
     let bonusLength = length - 6;
-    if(bonusLength > 0 && bonusLength < 6){
+    if (bonusLength > 0 && bonusLength < 6) {
         // 1.25 * n
-        if(hasUppercase){
+        if (hasUppercase) {
             return points = 1.25 * (bonusLength + 1);
 
         }
-        else{
+        else {
             return points = 1.25 * bonusLength;
         }
     }
-    else if(bonusLength >= 6 && bonusLength < 9){
+    else if (bonusLength >= 6 && bonusLength < 9) {
         // 1.75 * n
-        if(hasUppercase){
+        if (hasUppercase) {
             return points = 1.75 * (bonusLength + 1);
 
         }
-        else{
+        else {
             return points = 1.75 * bonusLength;
         }
     }
     // 2 * n
-    if(hasUppercase){
+    if (hasUppercase) {
         return points = 2 * (bonusLength + 1);
 
     }
-    else{
+    else {
         return points = 2 * bonusLength;
     }
 }
-function UpperCaseRatingByFeri(){
-    if(feriInput.value.toLowerCase() == feriInput.value){
+function UpperCaseRatingByFeri() {
+    if (feriInput.value.toLowerCase() == feriInput.value) {
         return false;
     }
     return true
 }
+function NumberRatingByFeri() {
+    if (/\d/.test(feriInput.value)) {
+        return 1.5;
+    }
+    return 0;
+}
+function SpecialRatingSexByFeri() {
+    let specials = /[^A-Za-z 0-9]/g;
+    if (specials.test(feriInput.value)) {
+        return 2;
+    }
+    uwu.nyah("Doesn't have special characters");
+
+    return 0;
+}
 
 feriInput.addEventListener("input", e => {
     FeriRating();
-});   
+});
 
 FeriRating(); //Start
